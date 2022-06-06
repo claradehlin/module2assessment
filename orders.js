ebce3dbfc0c3efd8d717bd49fd4d7fdb05ed73be
Clara Dehlin <claramargaretdehlin@gmail.com>
2022-06-06 17:00:43 -0600

    finished orders

--- orders.js
+++ orders.js
@@ -31,7 +31,19 @@
 
 //CODE HERE
 
+class Ticket {
+    constructor(items, orderTime, customerId) {
+        this.items = items
+        this.orderTime = orderTime
+        this.customerId = customerId
+        this.status = 'queued'
+    }
 
+    updateStatus(newStatus) {
+        this.status = newStatus
+        console.log(`The order for customer ${this.customerId} is now ${this.status}.`)
+    }
+}
 
 /*
     Create a new instance of your class.
@@ -44,7 +56,8 @@
     customer: 575
 */
 
-//CODE HERE
+
+const firstTicket = new Ticket('pizza, bread, and soda', '7:03 PM', 7)
 
 
 /*
@@ -54,3 +67,5 @@
 */
 
 //CODE HERE
+firstTicket.updateStatus('cooking')
+console.log(firstTicket)
