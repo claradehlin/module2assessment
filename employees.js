93fef14b781b1179cadade25482cbc674573fcea
Clara Dehlin <claramargaretdehlin@gmail.com>
2022-06-06 17:21:30 -0600

    finished test

--- employees.js
+++ employees.js
@@ -20,7 +20,16 @@
 */
 
 //CODE HERE
+class Employee {
+    constructor(name, shifts) {
+        this.name = name
+        this.shifts = shifts
+    }
 
+    getSchedule() {
+        console.log(`${this.name} works on ${this.shifts}`)
+    }
+}
 
 
 /*
@@ -34,6 +43,7 @@
 */
 
 //CODE HERE
+const empOne = new Employee('Clara', 'weekend nights, holidays')
 
 /*
     Call the `getSchedule` method on the
@@ -41,6 +51,7 @@
 */
 
 //CODE HERE
+empOne.getSchedule()
 
 
 /*
@@ -56,7 +67,12 @@
 */
 
 //CODE HERE
+// const empTwo = {...empOne}
+// console.log(empTwo)
+// console.log(empOne)
+// empTwo.name = 'Nick'
 
+const empTwo = {...empOne, name: 'Nick'}
 
 
 //////////////////PROBLEM 2////////////////////
@@ -83,7 +99,19 @@
 */
 
 //CODE HERE
+class Manager extends Employee {
+    constructor(name, shifts, employees) {
+        super(name, shifts)
+        this.employees = employees
+    }
 
+    getEmployees() {
+        console.log(`${this.name} manages ${this.employees}`)
+    }
+    addEmployee(emp) {
+        this.employees.push(emp)
+    }
+}
 
 
 /*
@@ -98,7 +126,7 @@
 */
 
 //CODE HERE
-
+const manager = new Manager('Johnny', 'all day every day', ['Sam', 'Bill'])
 
 /*
     Call the `getEmployees` method on the
@@ -106,6 +134,7 @@
 */
 
 //CODE HERE
+manager.getEmployees()
 
 /*
     Call the `addEmployee` method on the 
@@ -114,6 +143,7 @@
 */
 
 //CODE HERE 
+manager.addEmployee('Delilah')
 
 /*
     Call the `getEmployees` method on the
@@ -122,3 +152,4 @@
 */
 
 //CODE HERE
+manager.getEmployees()
